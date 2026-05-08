99a9db558bc1e65caed86d68359e7b5c8982439e
MayYTa <leonardo.juscamaytat@gmail.com>
2026-05-08 12:32:32 -0500

    chore : initial project structure

---
+++ nutrIA_api/src/main/java/com/nutria/nutria_api/NutrIaApiApplication.java
@@ -0,0 +1,13 @@
+package com.nutria.nutria_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrIaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutrIaApiApplication.class, args);
+    }
+
+}
