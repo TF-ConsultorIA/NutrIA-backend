3568fbd89700aaaeeebb1d654dd11790671745b8
Alexc0dex <mirandavivancoalexander@gmail.com>
2026-05-11 17:51:07 -0500

    feat: Global Exception handler & PageResponse added

---
+++ nutrIA_api/src/main/java/com/nutria/nutria_api/shared/exception/BusinessRuleException.java
@@ -0,0 +1,5 @@
+package com.nutria.nutria_api.shared.exception;
+
+public class BusinessRuleException extends RuntimeException {
+    public BusinessRuleException(String message) { super(message); }
+}
