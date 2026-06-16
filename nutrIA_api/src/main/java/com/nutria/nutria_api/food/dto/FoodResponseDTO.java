e03eaa617af5865739b420f9bc03beeaf3b31ced
Alexc0dex <mirandavivancoalexander@gmail.com>
2026-06-16 13:04:56 -0500

    feat: foodGroup property added into dto

--- nutrIA_api/src/main/java/com/nutria/nutria_api/food/dto/FoodResponseDTO.java
+++ nutrIA_api/src/main/java/com/nutria/nutria_api/food/dto/FoodResponseDTO.java
@@ -6,6 +6,7 @@ public record FoodResponseDTO(
         Long id,
         String foodName,
         FoodType foodType,
+        String foodGroup,
         Double energy,
         Double proteins,
         Double totalFat,
